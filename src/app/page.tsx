cb69421e637c34a35078649abf3ea00dc92f8f65
Inam <mfihassan99@gmail.com>
2025-11-22 23:33:08 +0530

    feat: add PostgreSQL support with Prisma integration and initial question dataset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import prisma from "./lib/prisma";
+
+export default async function Home() {
+  const questions = await prisma.question.findMany();
+  console.log(questions);
   return <div>Query Deck</div>;
 }
